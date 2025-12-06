c2d00c88e754c23644d01672107b0cb2f19fec1f
xmorand <60904575+xmorand@users.noreply.github.com>
2025-12-05 23:39:15 -0500

    Add handlers for RMS voltage, current, and metering

--- drivers/SmartThings/zigbee-switch/src/zigbee-switch-power/init.lua
+++ drivers/SmartThings/zigbee-switch/src/zigbee-switch-power/init.lua
@@ -8,6 +8,7 @@ local constants = require "st.zigbee.constants"
 local SimpleMetering = clusters.SimpleMetering
 local ElectricalMeasurement = clusters.ElectricalMeasurement
 local TemperatureMeasurement = clusters.TemperatureMeasurement
+local Metering = clusters.Metering
 
 local function active_power_meter_handler(driver, device, value, zb_rx)
   local raw_value = value.value
@@ -27,15 +28,40 @@ local function instantaneous_demand_handler(driver, device, value, zb_rx)
   device:emit_event(capabilities.powerMeter.power({value = raw_value, unit = "W"}))
 end
 
+local function rms_voltage_handler(driver, device, value, zb_rx)
+  device:emit_event(capabilities.voltageMeasurement.voltage({value = value.value, unit = "V"}))
+end
+
+local function rms_current_handler(driver, device, value, zb_rx)
+  device:emit_event(capabilities.currentMeter.current({value = value.value, unit = "A"}))
+end
+
+local function metering_handler(driver, device, value, zb_rx)
+  local raw_value = value.value / 1000  -- Convert Wh to kWh
+  device:emit_event(capabilities.energyMeter.energy({value = raw_value, unit = "kWh"}))
+end
+
+local function temperature_handler(driver, device, value, zb_rx)
+  device:emit_event(capabilities.temperatureMeasurement.temperature({value = value.value / 100, unit = "C"}))
+end
+
 local zigbee_switch_power = {
   NAME = "Zigbee Switch Power",
   zigbee_handlers = {
     attr = {
       [ElectricalMeasurement.ID] = {
-        [ElectricalMeasurement.attributes.ActivePower.ID] = active_power_meter_handler
+        [ElectricalMeasurement.attributes.ActivePower.ID] = active_power_meter_handler,
+        [ElectricalMeasurement.attributes.RmsVoltage.ID] = rms_voltage_handler,
+        [ElectricalMeasurement.attributes.RmsCurrent.ID] = rms_current_handler
       },
       [SimpleMetering.ID] = {
         [SimpleMetering.attributes.InstantaneousDemand.ID] = instantaneous_demand_handler
+      },
+      [Metering.ID] = {
+        [Metering.attributes.CumulativeEnergy.ID] = metering_handler
+      },
+      [TemperatureMeasurement.ID] = {
+        [TemperatureMeasurement.attributes.MeasuredValue.ID] = temperature_handler
       }
     }
   },
@@ -44,24 +70,3 @@ local zigbee_switch_power = {
 }
 
 return zigbee_switch_power
-
-
-local function electrical_measurement_handler(driver, device, value, raw)
-  if value.attr_id == 0x0400 then  -- Active Power
-    device:emit_event(capabilities.powerMeter.power({value = value.value, unit = "W"}))
-  elseif value.attr_id == 0x0505 then  -- RMS Voltage
-    device:emit_event(capabilities.voltageMeasurement.voltage({value = value.value, unit = "V"}))
-  elseif value.attr_id == 0x0508 then  -- RMS Current
-    device:emit_event(capabilities.currentMeter.current({value = value.value, unit = "A"}))
-  end
-end
-
-local function metering_handler(driver, device, value, raw)
-  if value.attr_id == 0x0000 then  -- Cumulative Energy
-    device:emit_event(capabilities.energyMeter.energy({value = value.value / 1000, unit = "kWh"}))
-  end
-end
-
-local function temperature_handler(driver, device, value, raw)
-  device:emit_event(capabilities.temperatureMeasurement.temperature({value = value.value / 100, unit = "C"}))
-end
